3169be1550f250b398cae0b8f416cfe3a58049ca
Tabio (God) <106675329+Endemic1337@users.noreply.github.com>
2023-07-29 18:42:05 +0200

    Removed Szypkos name

--- src/main/java/wtf/atani/utils/interfaces/ClientInformationAccess.java
+++ src/main/java/wtf/atani/utils/interfaces/ClientInformationAccess.java
@@ -2,6 +2,6 @@ package wtf.atani.utils.interfaces;
 
 public interface ClientInformationAccess {
     public static final String CLIENT_NAME = "Atani", VERSION = "0.0.1";
-    public static final String[] AUTHORS = new String[] {"Tábio", "Geuxy", "MarkGG", "Szypko", "Kellohylly"};
+    public static final String[] AUTHORS = new String[] {"Tábio", "Geuxy", "MarkGG", "Kellohylly"};
     public static final String PREFIX = "§c" + CLIENT_NAME + " §7>> ";
 }

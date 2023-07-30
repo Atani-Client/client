af9c13d171ccef7271e072effc160bb5c35b1aca
BooIMDD <86848614+MarkGG8181@users.noreply.github.com>
2023-07-30 23:07:56 +0300

    added .say <msg> command

---
+++ src/main/java/wtf/atani/command/impl/Say.java
@@ -0,0 +1,25 @@
+package wtf.atani.command.impl;
+
+import net.minecraft.network.play.client.C01PacketChatMessage;
+import wtf.atani.command.Command;
+import wtf.atani.command.data.CommandInfo;
+
+import java.util.Arrays;
+
+@CommandInfo(name = "say", description = "Say stuff")
+public class Say extends Command {
+    @Override
+    public boolean execute(String[] args) {
+        if (args.length == 1) {
+            String msg = args[0];
+            getPlayer().sendQueue.addToSendQueue(new C01PacketChatMessage(msg));
+            return true;
+        } else if (args.length == 0) {
+            sendHelp(this, "[Message]");
+        } else {
+            return false;
+        }
+
+        return true;
+    }
+}

6073ed65398635e4c38f9a9f6265901c450d2ec7
BooIMDD <86848614+MarkGG8181@users.noreply.github.com>
2023-07-31 10:42:05 +0300

    Added Keep Alive Disabler

---
+++ src/main/java/wtf/atani/module/impl/misc/Disabler.java
@@ -0,0 +1,35 @@
+package wtf.atani.module.impl.misc;
+
+import net.minecraft.network.Packet;
+import net.minecraft.network.play.client.C00PacketKeepAlive;
+import wtf.atani.event.events.PacketEvent;
+import wtf.atani.event.radbus.Listen;
+import wtf.atani.module.Module;
+import wtf.atani.module.data.ModuleInfo;
+import wtf.atani.module.data.enums.Category;
+import wtf.atani.value.impl.CheckBoxValue;
+
+@ModuleInfo(name = "Disabler", description = "Disable anticheats", category = Category.MISCELLANEOUS)
+public class Disabler extends Module {
+
+    private final CheckBoxValue keepAlive = new CheckBoxValue("C00KeepAlive", "Should the module cancel C00KeepAlive?", this, false);
+
+    @Listen
+    public void onPacketEvent(PacketEvent event) {
+        if(event.getType() == PacketEvent.Type.OUTGOING) {
+
+            Packet<?> packet = event.getPacket();
+
+            if(event.getPacket() instanceof C00PacketKeepAlive) {
+                event.setCancelled(true);
+            }
+        }
+    }
+
+    @Override
+    public void onEnable() {}
+
+    @Override
+    public void onDisable() {}
+
+}

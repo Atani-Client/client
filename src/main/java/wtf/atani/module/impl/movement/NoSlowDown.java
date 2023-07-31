e835b42afba1d504313dbd82966712907c94c98f
BooIMDD <86848614+MarkGG8181@users.noreply.github.com>
2023-07-31 10:16:30 +0300

    Added NCP, WatchDog & Old Intave NoSlow

--- src/main/java/wtf/atani/module/impl/movement/NoSlowDown.java
+++ src/main/java/wtf/atani/module/impl/movement/NoSlowDown.java
@@ -1,17 +1,118 @@
 package wtf.atani.module.impl.movement;
 
+import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.network.play.client.C07PacketPlayerDigging;
+import net.minecraft.network.play.client.C08PacketPlayerBlockPlacement;
+import net.minecraft.network.play.client.C09PacketHeldItemChange;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
+import wtf.atani.event.events.UpdateMotionEvent;
+import wtf.atani.event.radbus.Listen;
 import wtf.atani.module.Module;
 import wtf.atani.module.data.ModuleInfo;
 import wtf.atani.module.data.enums.Category;
+import wtf.atani.value.impl.StringBoxValue;
 
 @ModuleInfo(name = "NoSlowDown", description = "Removes the blocking & eating slowdown", category = Category.MOVEMENT)
 public class NoSlowDown extends Module {
 
     //Hooked in EntityLivingBase class & EntityPlayerSP class
 
+    private final StringBoxValue mode = new StringBoxValue("Mode", "Which mode will the module use?", this,  new String[]{"Vanilla", "Spoof", "NCP", "WatchDog", "Old Intave"});
+
+    // Spoof
+    private int spoofSlot;
+
+    // WatchDog
+    private int watchDogSlot;
+
+    @Listen
+    public void onMotionEvent(UpdateMotionEvent event) {
+        if(event.getType() == UpdateMotionEvent.Type.MID) {
+            switch(mode.getValue()) {
+                case "Vanilla":
+                    break;
+                case "Spoof":
+                    spoofSlot += 1;
+
+                    if(1 > spoofSlot || spoofSlot > 8) {
+                        spoofSlot = 1;
+                    }
+
+                    if(mc.thePlayer.inventory.currentItem == spoofSlot) {
+                        spoofSlot += 1;
+                    }
+
+                    if(mc.thePlayer.isUsingItem() || mc.thePlayer.isBlocking() || mc.thePlayer.isEating()) {
+                        mc.thePlayer.sendQueue.addToSendQueue(new C09PacketHeldItemChange(spoofSlot));
+                    }
+                    break;
+                case "NCP":
+                    if (mc.thePlayer.isBlocking()) {
+                        mc.getNetHandler().addToSendQueue(new C07PacketPlayerDigging(C07PacketPlayerDigging.Action.RELEASE_USE_ITEM, BlockPos.ORIGIN, EnumFacing.DOWN));
+                    }
+                    break;
+                case "WatchDog":
+                    if(this.isMoving()) {
+                        if (mc.thePlayer.isBlocking()) {
+                            spoofSlot += 1;
+
+                            if (1 > spoofSlot || spoofSlot > 8) {
+                                spoofSlot = 1;
+                            }
+
+                            if (mc.thePlayer.inventory.currentItem == spoofSlot) {
+                                spoofSlot += 1;
+                            }
+
+                            mc.thePlayer.sendQueue.addToSendQueue(new C09PacketHeldItemChange(spoofSlot));
+                        }
+
+                        if(mc.thePlayer.isEating() || mc.thePlayer.isUsingItem() && !mc.thePlayer.isBlocking()) {
+                            if(mc.thePlayer.ticksExisted % 2 + Math.round(Math.random()) == 0) {
+                                mc.thePlayer.sendQueue.addToSendQueue(new C08PacketPlayerBlockPlacement(null));
+                            }
+                        }
+                    }
+                    break;
+                case "Old Intave":
+                    if (mc.thePlayer.isUsingItem()) {
+                        mc.getNetHandler().addToSendQueue(new C09PacketHeldItemChange(getIndexOfItem() % 8 + 1));
+                        mc.getNetHandler().addToSendQueue(new C09PacketHeldItemChange(getIndexOfItem()));
+                    }
+                    break;
+            }
+        }
+
+        if(event.getType() == UpdateMotionEvent.Type.POST) {
+            switch(mode.getValue()) {
+                case "NCP":
+                    if (mc.thePlayer.isBlocking()) {
+                        mc.getNetHandler().addToSendQueue(new C08PacketPlayerBlockPlacement(mc.thePlayer.inventory.getCurrentItem()));
+                    }
+                    break;
+                case "Old Intave":
+                    if (mc.thePlayer.isUsingItem()) {
+                        mc.getNetHandler().addToSendQueue(new C08PacketPlayerBlockPlacement(getItemStack()));
+                    }
+                    break;
+            }
+        }
+    }
+
     @Override
     public void onEnable() {}
 
     @Override
     public void onDisable() {}
+
+    private int getIndexOfItem() {
+        final InventoryPlayer inventoryPlayer = mc.thePlayer.inventory;
+        return inventoryPlayer.currentItem;
+    }
+
+    private ItemStack getItemStack() {
+        return (mc.thePlayer == null || mc.thePlayer.inventoryContainer == null ? null : mc.thePlayer.inventoryContainer.getSlot(getIndexOfItem() + 36).getStack());
+    }
 }

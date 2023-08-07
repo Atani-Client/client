ed5243f995f01a07628278279d7164c5b03c7935
Tabio (God) <106675329+Endemic1337@users.noreply.github.com>
2023-08-07 17:05:36 +0200

    New TickBase and cleaned up Blink

--- src/main/java/wtf/atani/module/impl/misc/Blink.java
+++ src/main/java/wtf/atani/module/impl/misc/Blink.java
@@ -17,12 +17,10 @@ import java.util.ArrayDeque;
 @ModuleInfo(name = "Blink", description = "Blocks your packets for a time being", category = Category.MISCELLANEOUS, alwaysEnabled = true)
 public class Blink extends Module {
 
-    private final ArrayDeque<Packet> inPacketDeque = new ArrayDeque<>();
     private final ArrayDeque<Packet> outPacketDeque = new ArrayDeque<>();
     private final TimeHelper fakeLagTimer = new TimeHelper();
 
     private final CheckBoxValue incoming = new CheckBoxValue("Incoming", "Queue incoming packets?", this, false);
-    private final CheckBoxValue burst = new CheckBoxValue("Burst", "Burst out packets after disabling?", this, true);
     private final CheckBoxValue pulse = new CheckBoxValue("Pulse", "Disable and enable the module every x amount of ms?", this, false);
     private final SliderValue<Long> pulseDelay = new SliderValue<>("Pulse Delay", "What'll be the delay between pulsing?", this, 150L, 50L, 5000L, 1);
 
@@ -31,18 +29,9 @@ public class Blink extends Module {
     @Listen
     public void onUpdate(UpdateEvent updateEvent) {
         if(active && !this.isEnabled()) {
-            if(burst.getValue()) {
-                while (!outPacketDeque.isEmpty()) {
-                    sendPacketUnlogged(outPacketDeque.poll());
-                }
-            } else {
-                // To prevent server from tping back (this doesn't actually work lmfao)
-                sendPacket(new C03PacketPlayer());
+            while (!outPacketDeque.isEmpty()) {
+                sendPacketUnlogged(outPacketDeque.poll());
             }
-            while (!inPacketDeque.isEmpty()) {
-                mc.getNetHandler().addToReceiveQueue(inPacketDeque.poll());
-            }
-            outPacketDeque.clear();
             active = false;
         }
     }
@@ -50,17 +39,10 @@ public class Blink extends Module {
     @Listen
     public void onPacket(PacketEvent event) {
         if (active && (event.getType() == PacketEvent.Type.OUTGOING || incoming.getValue())) {
-            if(event.getType() == PacketEvent.Type.INCOMING) {
-                inPacketDeque.add(event.getPacket());
-            } else {
-                outPacketDeque.add(event.getPacket());
-            }
+            outPacketDeque.add(event.getPacket());
             if (pulse.getValue() && fakeLagTimer.hasReached(pulseDelay.getValue().longValue())) {
                 while (!outPacketDeque.isEmpty()) {
                     sendPacketUnlogged(outPacketDeque.poll());
-                }
-                while (!inPacketDeque.isEmpty()) {
-                    mc.getNetHandler().addToReceiveQueue(inPacketDeque.poll());
                 }
                 fakeLagTimer.reset();
             }

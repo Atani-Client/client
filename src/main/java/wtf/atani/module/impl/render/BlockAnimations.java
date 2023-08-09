e17f7b4ef8ea09a3f2eb63bf365e408f79267772
BooIMDD <86848614+MarkGG8181@users.noreply.github.com>
2023-08-09 18:38:08 +0300

    Added Exhibition Block Animation

--- src/main/java/wtf/atani/module/impl/render/BlockAnimations.java
+++ src/main/java/wtf/atani/module/impl/render/BlockAnimations.java
@@ -3,6 +3,7 @@ package wtf.atani.module.impl.render;
 import wtf.atani.module.Module;
 import wtf.atani.module.data.ModuleInfo;
 import wtf.atani.module.data.enums.Category;
+import wtf.atani.value.impl.CheckBoxValue;
 import wtf.atani.value.impl.StringBoxValue;
 
 @ModuleInfo(name = "BlockAnimations", description = "Changes your item blocking animation", category = Category.RENDER)
@@ -10,7 +11,8 @@ public class BlockAnimations extends Module {
 
     //Hooked in ItemRenderer class && Minecraft class
 
-    public final StringBoxValue mode = new StringBoxValue("Style", "What animation should the sword use?", this, new String[]{"1.7", "Atani"});
+    public final StringBoxValue mode = new StringBoxValue("Style", "What animation should the sword use?", this, new String[]{"1.7", "Atani", "Exhibition"});
+    public final CheckBoxValue blockHit = new CheckBoxValue("Block Hit", "Should the sword allow block hitting?", this, true);
 
     @Override
     public void onEnable() {}

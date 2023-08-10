bfa1a11e3d6447eb5f0dad0f1c7708bbb0441d6f
Tabio (God) <106675329+Endemic1337@users.noreply.github.com>
2023-08-10 05:24:41 +0200

    wtf

--- src/main/java/wtf/atani/module/impl/combat/Extinguish.java
+++ src/main/java/wtf/atani/module/impl/combat/Extinguish.java
@@ -1,18 +1,41 @@
 package wtf.atani.module.impl.combat;
 
+import net.minecraft.block.material.Material;
 import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
+import net.minecraft.network.play.client.C0APacketAnimation;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.MovingObjectPosition;
+import org.lwjgl.Sys;
+import wtf.atani.event.events.ClickingEvent;
+import wtf.atani.event.events.RotationEvent;
 import wtf.atani.event.events.UpdateEvent;
 import wtf.atani.event.radbus.Listen;
 import wtf.atani.module.Module;
 import wtf.atani.module.data.ModuleInfo;
 import wtf.atani.module.data.enums.Category;
+import wtf.atani.utils.player.PlayerHandler;
 
 @ModuleInfo(name = "Extinguish", description = "Automatically extinguishes fire under you", category = Category.COMBAT)
 public class Extinguish extends Module {
 
     @Listen
     public void onUpdate(UpdateEvent updateEvent) {
+        if(mc.thePlayer.isBurning()) {
+            for (int i = 0; i < InventoryPlayer.getHotbarSize(); i++) {
+
+                ItemStack itemStack = mc.thePlayer.inventory.getStackInSlot(i);
+                if (itemStack != null && itemStack.getItem().getIdFromItem(itemStack.getItem()) == 326) {
+                    mc.thePlayer.inventory.currentItem = i;
+                }
+            }
+        }
+    }
+
+    @Listen
+    public void onRotate(RotationEvent rotationEvent) {
         if (mc.thePlayer.isBurning()) {
             for (int i = 0; i < InventoryPlayer.getHotbarSize(); i++) {
 
@@ -25,10 +48,39 @@ public class Extinguish extends Module {
                 return;
             }
             if (mc.thePlayer.getHeldItem().getItem().getIdFromItem(mc.thePlayer.getHeldItem().getItem()) == 326) {
-                float oldpitch = mc.thePlayer.rotationPitch;
-                mc.thePlayer.rotationPitch = 90f;
-                mc.playerController.sendUseItem(mc.thePlayer, mc.theWorld, mc.thePlayer.getHeldItem());
-                mc.thePlayer.rotationPitch = oldpitch;
+                rotationEvent.setPitch(90);
+            }
+        }
+    }
+
+    @Listen
+    public void onClicking(ClickingEvent clickingEvent) {
+        if (mc.thePlayer.isBurning()) {
+            if(mc.thePlayer.getHeldItem() == null) {
+                return;
+            }
+            if (mc.thePlayer.getHeldItem().getItem().getIdFromItem(mc.thePlayer.getHeldItem().getItem()) == 326) {
+                if(mc.thePlayer == null || mc.theWorld == null)
+                    return;
+
+                MovingObjectPosition objectOver = mc.objectMouseOver;
+                BlockPos blockpos = mc.objectMouseOver.getBlockPos();
+                ItemStack itemstack = mc.thePlayer.inventory.getCurrentItem();
+
+                if (objectOver.typeOfHit != MovingObjectPosition.MovingObjectType.BLOCK || mc.theWorld.getBlockState(blockpos).getBlock().getMaterial() == Material.air) {
+                    return;
+                }
+
+                sendMessage("Trying to place");
+                if (mc.playerController.onPlayerRightClick(mc.thePlayer, mc.theWorld, itemstack, blockpos, objectOver.sideHit, objectOver.hitVec)) {
+                    sendMessage("Placed ig");
+                    mc.thePlayer.sendQueue.addToSendQueue(new C0APacketAnimation());
+                }
+                if (itemstack != null && itemstack.stackSize == 0) {
+                    mc.thePlayer.inventory.mainInventory[mc.thePlayer.inventory.currentItem] = null;
+                }
+
+                mc.sendClickBlockToController(mc.currentScreen == null && mc.gameSettings.keyBindAttack.isKeyDown() && mc.inGameHasFocus);
             }
         }
     }

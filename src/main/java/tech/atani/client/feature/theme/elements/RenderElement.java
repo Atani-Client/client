d71b4a5fb30eb3a5fd4dad2476b3d230f201318e
Tabio (God) <106675329+Endemic1337@users.noreply.github.com>
2023-08-20 23:06:20 +0200

    Will finish this when I won't be dumb

---
+++ src/main/java/tech/atani/client/feature/theme/elements/RenderElement.java
@@ -0,0 +1,7 @@
+package tech.atani.client.feature.theme.elements;
+
+public interface RenderElement {
+
+    void draw();
+
+}
---
+++ src/main/java/tech/atani/client/feature/theme/storage/ThemeStorage.java
@@ -0,0 +1,13 @@
+package tech.atani.client.feature.theme.storage;
+
+import de.florianmichael.rclasses.storage.Storage;
+import tech.atani.client.feature.theme.Theme;
+
+public class ThemeStorage extends Storage<Theme> {
+
+    @Override
+    public void init() {
+
+    }
+
+}

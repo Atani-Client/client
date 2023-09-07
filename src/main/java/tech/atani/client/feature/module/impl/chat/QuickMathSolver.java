61702c2def798de375dc88518c6882a3b7a25751
Tabio (God) <106675329+Endemic1337@users.noreply.github.com>
2023-09-07 15:45:57 +0200

    Split modules

---
+++ src/main/java/tech/atani/client/feature/module/impl/chat/QuickMathSolver.java
@@ -0,0 +1,50 @@
+package tech.atani.client.feature.module.impl.chat;
+
+import net.minecraft.network.play.client.C01PacketChatMessage;
+import net.minecraft.network.play.server.S02PacketChat;
+import net.minecraft.util.EnumChatFormatting;
+import tech.atani.client.feature.module.Module;
+import tech.atani.client.feature.module.data.ModuleData;
+import tech.atani.client.feature.module.data.enums.Category;
+import tech.atani.client.listener.event.minecraft.network.PacketEvent;
+import tech.atani.client.listener.radbus.Listen;
+import tech.atani.client.utility.interfaces.Methods;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+@ModuleData(name = "QuickMathSolver", description = "Automatically solves quick maths", category = Category.CHAT)
+public class QuickMathSolver extends Module {
+
+    @Listen
+    public void onPacket(PacketEvent event) {
+        if(Methods.mc.thePlayer == null || Methods.mc.theWorld == null)
+            return;
+
+        if(event.getPacket() instanceof S02PacketChat) {
+            String unformatted = EnumChatFormatting.getTextWithoutFormattingCodes(((S02PacketChat)event.getPacket()).getChatComponent().getUnformattedText()), text = unformatted.replace(" ", "");
+            if(unformatted.contains("QUICK MATHS! Solve: ")) {
+                String[] array = ((S02PacketChat) event.getPacket()).getChatComponent().getUnformattedText().split("Solve: ");
+                ScriptEngineManager mgr = new ScriptEngineManager();
+                ScriptEngine engine = mgr.getEngineByName("JavaScript");
+
+                try {
+                    sendPacketUnlogged(new C01PacketChatMessage(engine.eval(array[1].replace("x", "*")).toString()));
+                } catch (ScriptException he) {
+                    he.printStackTrace();
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}

f3fe3f79ec4da266de3dc417da2fa3d808a4bff6
Mark <86848614+MarkGG8181@users.noreply.github.com>
2023-09-11 20:12:10 +0300

    Removed Tabio from authors

--- src/main/java/tech/atani/client/utility/interfaces/ClientInformationAccess.java
+++ src/main/java/tech/atani/client/utility/interfaces/ClientInformationAccess.java
@@ -2,7 +2,7 @@ package tech.atani.client.utility.interfaces;
 
 public interface ClientInformationAccess {
     String CLIENT_NAME = "Atani", CLIENT_VERSION = "0.0.1", CLIENT_NAME_JAPANASE = "アタニ";
-    String[] AUTHORS = new String[] {"Tábio", "MarkGG", "Geuxy", "Kellohylly", "Liticane", "Exterminate", "mistakedev"};
+    String[] AUTHORS = new String[] {"MarkGG", "Geuxy", "Kellohylly", "Liticane", "Exterminate", "mistakedev"};
     String PREFIX = "§c" + CLIENT_NAME + " §7>> ";
     boolean DEVELOPMENT_SWITCH = true, BETA_SWITCH = true;
     String APIURL = "https://api.menaceclient.me/";

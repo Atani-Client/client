73ef39acda4e071435a3bcc43d48542b3c696d87
Kellohylly <125300526+Kellohylly@users.noreply.github.com>
2023-09-19 19:41:34 +0300

    grim ab and tried to work on legit hitboxes

--- src/main/java/tech/atani/client/feature/module/impl/combat/HitBoxes.java
+++ src/main/java/tech/atani/client/feature/module/impl/combat/HitBoxes.java
@@ -2,8 +2,10 @@ package tech.atani.client.feature.module.impl.combat;
 
 import com.google.common.base.Supplier;
 import com.viaversion.viabackwards.utils.ChatUtil;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.MovingObjectPosition;
 import tech.atani.client.feature.module.Module;
 import tech.atani.client.feature.module.data.ModuleData;
 import tech.atani.client.feature.module.data.enums.Category;
@@ -14,6 +16,8 @@ import tech.atani.client.feature.value.impl.StringBoxValue;
 import tech.atani.client.listener.event.minecraft.player.rotation.RayTraceRangeEvent;
 import tech.atani.client.listener.event.minecraft.player.rotation.RotationEvent;
 import tech.atani.client.listener.radbus.Listen;
+import tech.atani.client.utility.interfaces.Methods;
+import tech.atani.client.utility.player.PlayerHandler;
 import tech.atani.client.utility.player.combat.FightUtil;
 import tech.atani.client.utility.player.rotation.RotationUtil;
 
@@ -25,17 +29,24 @@ public class HitBoxes extends Module {
 
     public StringBoxValue hitBoxMode = new StringBoxValue("HitBox Mode", "Which mode should hitbox use?", this, new String[] {"Normal", "Legit"});
 
+    private float yawRot;
+    private float pitchRot;
     @Listen
     public final void onRotation(RotationEvent rotationEvent) {
         if(hitBoxMode.is("Legit")) {
             //fix range!
             // Killaura, gotta make with these List<EntityLivingBase> targets = FightUtil.getMultipleTargets(findRange.getValue(), players.getValue(), animals.getValue(), walls.getValue(), monsters.getValue(), invisible.getValue());
             List<EntityLivingBase> targets = FightUtil.getMultipleTargets(3.0, true, false, false, true, true);
-            
-            final float[] rotations = RotationUtil.getRotation(targets.get(1), "Bruteforce", 0, true, false, 0, 0, 0, 0, true, 0, 0, 0, 0, false, false);
+            if (targets.get(1) != null) {
+                final float[] rotations = RotationUtil.getRotation(targets.get(1), "Bruteforce", 0, true, true, 0, 0, 0, 0, false, 40, 40, 40, 40, true, true);
 
-            if(mc.gameSettings.keyBindAttack.pressed)
-                mc.thePlayer.sendChatMessage("rots: " + Arrays.toString(rotations));
+                yawRot = rotations[0];
+                pitchRot = rotations[1];
+
+                mc.thePlayer.rotationYawHead = yawRot;
+                rotationEvent.setYaw(yawRot);
+                rotationEvent.setPitch(pitchRot);
+            }
         }
     }
     @Override

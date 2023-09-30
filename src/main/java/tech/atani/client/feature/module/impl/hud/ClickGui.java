2eefff7d5d8f4eb4b2a7ea92800ddac9ad7a7097
BooIMDD <86848614+MarkGG8181@users.noreply.github.com>
2023-09-30 11:50:54 +0300

    added back astolfo clickgui and fixed it :D

--- src/main/java/tech/atani/client/feature/module/impl/hud/ClickGui.java
+++ src/main/java/tech/atani/client/feature/module/impl/hud/ClickGui.java
@@ -2,6 +2,7 @@ package tech.atani.client.feature.module.impl.hud;
 
 import com.google.common.base.Supplier;
 import org.lwjgl.input.Keyboard;
+import tech.atani.client.feature.guis.screens.clickgui.astolfo.AstolfoClickGuiScreen;
 import tech.atani.client.feature.guis.screens.clickgui.atani.AtaniClickGuiScreen;
 import tech.atani.client.feature.guis.screens.clickgui.augustus.AugustusClickGuiScreen;
 import tech.atani.client.feature.guis.screens.clickgui.fatality.FatalityClickGuiScreen;
@@ -23,7 +24,7 @@ import tech.atani.client.feature.value.impl.StringBoxValue;
 @ModuleData(name = "ClickGui", description = "A clicky gui", category = Category.HUD, key = Keyboard.KEY_RSHIFT)
 public class ClickGui extends Module {
 
-    public final StringBoxValue mode = new StringBoxValue("Mode", "Which mode will the module use?", this, new String[]{"Atani Simple", "Atani CS:GO", "Atani Golden", "Augustus", "Augustus 2.6", "Xave", "Ryu", "Icarus", "Fatality", "Koks", "Tarasande"});
+    public final StringBoxValue mode = new StringBoxValue("Mode", "Which mode will the module use?", this, new String[]{"Atani Simple", "Atani CS:GO", "Atani Golden", "Augustus", "Augustus 2.6", "Xave", "Ryu", "Icarus", "Fatality", "Koks", "Tarasande", "Astolfo"});
     public final CheckBoxValue openingAnimation = new CheckBoxValue("Opening Animation", "Animate the opening and closing of the gui?", this, true);
     public final StringBoxValue dropdownAnimation = new StringBoxValue("Animation Mode", "How will the opening animation look like?", this, new String[]{"Scale-In", "Frame Scale-In", "Left to Right", "Right to Left", "Up to Down", "Down to Up"}, new Supplier[]{() ->
             mode.getValue().equalsIgnoreCase("Atani Simple") || // Dropdown guis go here
@@ -31,6 +32,7 @@ public class ClickGui extends Module {
             mode.getValue().equalsIgnoreCase("Augustus 2.6") ||
             mode.getValue().equalsIgnoreCase("Xave") ||
             mode.getValue().equalsIgnoreCase("Tarasande") ||
+            mode.getValue().equalsIgnoreCase("Astolfo") ||
             mode.getValue().equalsIgnoreCase("Icarus")}).setIdName("Dropdown Animation Mode");
     public final StringBoxValue nonDropdownAnimation = new StringBoxValue("Animation Mode", "How will the opening animation look like?", this, new String[]{"Left to Right", "Right to Left", "Up to Down", "Down to Up"}, new Supplier[]{() ->
             mode.getValue().equalsIgnoreCase("Atani Golden") || // Non-Dropdown guis go here
@@ -53,10 +55,17 @@ public class ClickGui extends Module {
     public static IcarusClickGuiScreen clickGuiScreenIcarus;
     public static KoksClickGuiScreen clickGuiScreenKoks;
     public static TarasandeClickGuiScreen clickGuiScreenTarasande;
+    public static AstolfoClickGuiScreen clickGuiScreenAstolfo;
 
     @Override
     public void onEnable() {
         switch (this.mode.getValue()) {
+            case "Astolfo":
+                if(clickGuiScreenAstolfo == null) {
+                    clickGuiScreenAstolfo = new AstolfoClickGuiScreen();
+                }
+                mc.displayGuiScreen(clickGuiScreenAstolfo);
+                break;
             case "Tarasande":
                 if(clickGuiScreenTarasande == null) {
                     clickGuiScreenTarasande = new TarasandeClickGuiScreen();

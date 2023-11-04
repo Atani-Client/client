59ff6c47bc48474a3f87c61df5e0eb7ea9e8ee93
Kellohylly <125300526+Kellohylly@users.noreply.github.com>
2023-11-04 21:48:28 +0200

    add antiaura

---
+++ src/main/java/tech/atani/client/feature/module/impl/misc/AntiAura.java
@@ -0,0 +1,50 @@
+package tech.atani.client.feature.module.impl.misc;
+
+import cn.muyang.nativeobfuscator.Native;
+import net.minecraft.network.Packet;
+import net.minecraft.network.play.client.C03PacketPlayer;
+import tech.atani.client.feature.module.Module;
+import tech.atani.client.feature.module.data.ModuleData;
+import tech.atani.client.feature.module.data.enums.Category;
+import tech.atani.client.listener.event.minecraft.network.PacketEvent;
+import tech.atani.client.listener.event.minecraft.player.movement.UpdateEvent;
+import tech.atani.client.listener.radbus.Listen;
+import tech.atani.client.utility.math.time.TimeHelper;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+@Native
+@ModuleData(name = "AntiAura", description = "Can cause enemies aura to break", category = Category.MISCELLANEOUS)
+public class AntiAura extends Module {
+    private final TimeHelper timeHelper = new TimeHelper();
+    private final List<Packet<?>> packetsList = new CopyOnWriteArrayList<>();
+    @Listen
+    public void onPacket(PacketEvent event) {
+        if(event.getPacket() instanceof C03PacketPlayer && active)
+            event.setCancelled(true);
+    }
+
+    @Listen
+    public void onUpdate(UpdateEvent event) {
+        if (timeHelper.hasReached(50, true)) {
+            if (!packetsList.isEmpty()) {
+                for (Packet<?> packet : packetsList) {
+                    mc.getNetHandler().addToSendQueue(packet);
+                    packetsList.remove(packet);
+                }
+            }
+            timeHelper.reset();
+        }
+    }
+    @Override
+    public void onEnable() {
+        active = true;
+    }
+
+    @Override
+    public void onDisable() {
+        active = false;
+    }
+
+}
